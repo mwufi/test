4490a03b070c3977836b29ce784fb62882de70c7
Zen Tang <zen@wayve.ai>
2020-03-04 18:01:11 -0600

    minor

--- data.py
+++ data.py
@@ -71,15 +71,15 @@ def make_dataset(op):
     print('Downloading data...')
 
     if op.dataset == 'celeba':
-        op.update({'nc': 3})
+        op.update({'nc': 3}, allow_val_change=True )
         return dset.CelebA(root='celeba', download=True, transform=make_transforms(op))
 
     elif op.dataset == 'fashion_mnist':
-        op.update({'nc': 3})
+        op.update({'nc': 3}, allow_val_change=True )
         return dset.FashionMNIST(root='fashion_mnist', download=True, transform=make_transforms(op))
 
     elif op.dataset == 'pokemon':
-        op.update({'nc': 3})
+        op.update({'nc': 3}, allow_val_change=True )
         return PokeSprites(op)
 
     else:
