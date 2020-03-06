a3474cf0407708023e781ba8e779b5493eeed8fd
Zen Tang <zen@wayve.ai>
2020-03-05 19:30:57 -0600

    minor

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,5 @@
 import torch
 import torchvision.datasets as dset
-import wandb
-from torchvision import utils as vutils
 
 from .pokemon import PokeSprites
 from .utils import make_transforms, infinite_data
@@ -35,13 +33,5 @@ def create_train_data(op, device):
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=op.data.loader.batch_size,
                                              shuffle=True, num_workers=op.data.loader.workers)
 
-    # Plot some training images
-    real_batch = next(iter(dataloader))
-
-    wandb.log({
-        'Training images': wandb.Image(vutils.make_grid(real_batch[0].to(device)[:64],
-                                                        padding=2,
-                                                        normalize=True).cpu())
-    })
-
+    # Make it never stop
     return infinite_data(dataloader, device)
